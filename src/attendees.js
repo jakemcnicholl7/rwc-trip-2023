cd9ae6fde3876243c9c0a5da2f2d1ed7715a6ae6
Jake McNicholl <jakemcnicholl@gmail.com>
2023-05-02 19:26:09 +0100

    update attendees: add Conor to South Africa vs Tonga

--- src/attendees.js
+++ src/attendees.js
@@ -2,7 +2,7 @@
 const attendees = {
     "saint_etiene": ["Jake", "Jass", "Dan", "Katie"],
     "lyon": ["Jake", "Jass", "Dan", "Katie"],
-    "marseille": ["Jake", "Jass", "Dan", "Katie"]
+    "marseille": ["Jake", "Jass", "Dan", "Katie", "Conor"]
 }
 
 export default attendees;
