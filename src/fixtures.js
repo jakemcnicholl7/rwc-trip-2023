d1cb5777afd6d540c7ea802c0dc3894fb58a7f82
Jake McNicholl <jakemcnicholl@gmail.com>
2023-05-02 19:23:54 +0100

    fix incorrect match dates

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,8 +1,8 @@
 import Match from "./match.js"
 
 const fixtures = [
-    new Match("Argentina","Samoa","Thu 21st Sep", "17:45", "saint_etiene", 6),
-    new Match("Uruguay", "Namibia", "Wed 28th Sep", "17:45", "lyon", 6),
+    new Match("Argentina","Samoa","Fri 22nd Sep", "17:45", "saint_etiene", 6),
+    new Match("Uruguay", "Namibia", "Wed 27th Sep", "17:45", "lyon", 6),
     new Match("South Africa", "Tonga", "Sun 1st Oct", "21:00", "marseille", 6)
 ]
 
